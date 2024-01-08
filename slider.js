bdb5fddef38ab90b1baac84419e28465898a5410
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-01-08 15:01:06 +0530

    approx

---
+++ slider.js
@@ -0,0 +1,44 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].clientWidth;
+const slidesPerPage = 1; // Display 3 slides at a time
+let slideIndex;
+
+function nextSlide() {
+    if (slideIndex < (slides.length - 2) - slidesPerPage) {
+        slideIndex++;
+    } else {
+        slideIndex = 0;
+        slider.style.transition = 'none';
+        slider.style.transform = `translateX(0)`;
+        setTimeout(() => {
+            slider.style.transition = '';
+        }, 2000);
+    }
+    slider.style.transform = `translateX(-${slideIndex * slideWidth * slidesPerPage}px)`;
+}
+
+setInterval(nextSlide, 2000); // Change slide every 3 seconds (3000 milliseconds)
+
+
+const owlslider = document.querySelector('.owlslider');
+const owlslides = document.querySelectorAll('.owlslide');
+const owlslideWidth = owlslides[0].clientWidth;
+const owlslidesPerPage = 1; // Display 3 slides at a time
+let owlslideIndex;
+
+function newSlide() {
+    if (owlslideIndex < owlslides.length - owlslidesPerPage) {
+        owlslideIndex++;
+    } else {
+        owlslideIndex = 0;
+        owlslider.style.transition = 'none';
+        owlslider.style.transform = `translateX(0)`;
+        setTimeout(() => {
+            owlslider.style.transition = '';
+        }, 2000);
+    }
+    owlslider.style.transform = `translateX(-${owlslideIndex * owlslideWidth * owlslidesPerPage}px)`;
+}
+
+setInterval(newSlide, 2000); // Change slide every 3 seconds (3000 milliseconds)
